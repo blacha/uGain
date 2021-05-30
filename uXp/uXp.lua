dc9dac4281372dda1122eed3d5b7d73ea31b6faa
Blayne Chard <blayne@chard.com>
2021-05-30 17:11:46 +1200

    refactor: ensure everything is renamed

--- uXp/uXp.lua
+++ uXp/uXp.lua
@@ -1,5 +1,5 @@
-local uExp_CurrentExp = 0
-local uExp_CurrentLevel = 0
+local uXp_CurrentExp = 0
+local uXp_CurrentLevel = 0
 
 local C = {
     light = "|cFFABD6F4",
@@ -8,7 +8,7 @@ local C = {
     resume = "|r"
 }
 
-local function uExp_FormatChatMessage(xpChange, currentXp, nextLevelXp, nextLevel)
+local function uXp_FormatChatMessage(xpChange, currentXp, nextLevelXp, nextLevel)
     local remainingXp = nextLevelXp - currentXp
     local repsToNextLevel = floor(remainingXp / xpChange) + 1
 
@@ -21,13 +21,13 @@ local function uExp_FormatChatMessage(xpChange, currentXp, nextLevelXp, nextLeve
     return xpGain .. xpToNextLevel .. xpLeft .. xpReps
 end
 
-local function uExp_ScanAndReportExp()
+local function uXp_ScanAndReportExp()
     local currentXp = UnitXP("player")
     local nextLevelXp = UnitXPMax("player")
     local currentLevel = UnitLevel("player")
 
-    local xpChange = currentXp - uExp_CurrentExp
-    uExp_CurrentExp = currentXp
+    local xpChange = currentXp - uXp_CurrentExp
+    uXp_CurrentExp = currentXp
 
     -- Xp was reset skip..
     if (xpChange == 0) then
@@ -35,12 +35,12 @@ local function uExp_ScanAndReportExp()
     end
 
     -- Level change occured dont message anything
-    if currentLevel ~= uExp_CurrentLevel then
-        uExp_CurrentLevel = currentLevel 
+    if currentLevel ~= uXp_CurrentLevel then
+        uXp_CurrentLevel = currentLevel 
         return
     end
 
-    local chatMessage = uExp_FormatChatMessage(xpChange, currentXp, nextLevelXp, currentLevel + 1)
+    local chatMessage = uXp_FormatChatMessage(xpChange, currentXp, nextLevelXp, currentLevel + 1)
     uShared_PrintAll("CHAT_MSG_COMBAT_XP_GAIN", chatMessage)
 end
 
@@ -51,18 +51,18 @@ eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 eventFrame:SetScript("OnEvent", function(self, event, data)
     print("Hello")
     if event == "PLAYER_ENTERING_WORLD" then
-        uExp_CurrentExp = UnitXP("player")
-        uExp_CurrentLevel = UnitLevel("player")
-        uExp_ScanAndReportExp()
+        uXp_CurrentExp = UnitXP("player")
+        uXp_CurrentLevel = UnitLevel("player")
+        uXp_ScanAndReportExp()
     elseif event == "PLAYER_XP_UPDATE" then
-        uExp_ScanAndReportExp()
+        uXp_ScanAndReportExp()
     end
 end)
 
 -- Filter out XP Gains
-local function uExp_FilterCombatXpGain(self, event, msg, ...)
+local function uXp_FilterCombatXpGain(self, event, msg, ...)
     return true
 end
-ChatFrame_AddMessageEventFilter("CHAT_MSG_COMBAT_XP_GAIN", uExp_FilterCombatXpGain)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_COMBAT_XP_GAIN", uXp_FilterCombatXpGain)
 
 -- "Greater Diskbat dies, you gain 142 experience (+71 Rested bounus)"
