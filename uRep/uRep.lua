21111ea980cc08752ff798644889cea572a1ad75
Blayne Chard <blayne@chard.com>
2021-06-11 19:55:34 +1200

    feat(uRep): show when new factions are discovered

--- uRep/uRep.lua
+++ uRep/uRep.lua
@@ -16,10 +16,10 @@ local FACTION_BAR_COLORS = {
     [8] = "|cffbfb2f3" -- 1000 Exalted - Purple
 };
 
-local factions = {}
+local Factions = {}
 local FactionCount = 0
-local standingMax = 8
-local standingMin = 1
+local StandingMax = 8
+local StandingMin = 1
 
 local function uRep_StandingText(standingId)
     return uC(_G["FACTION_STANDING_LABEL" .. standingId], FACTION_BAR_COLORS[standingId])
@@ -32,7 +32,7 @@ local function uRep_ReportFaction(factionIndex)
     end
 
     -- Unknwon faction ??
-    local faction = factions[name]
+    local faction = Factions[name]
     if not faction then
         return nil
     end
@@ -55,13 +55,13 @@ local function uRep_ReportFaction(factionIndex)
 
     if diff > 0 then
         remaining = barMax - currentRep
-        if standingId < standingMax then
+        if standingId < StandingMax then
             nextStandingId = standingId + 1
         end
     else
         colorChange = C.red
         remaining = currentRep - barMin
-        if standingId > standingMin then
+        if standingId > StandingMin then
             nextStandingId = standingId - 1
         end
     end
@@ -79,23 +79,32 @@ local function uRep_ReportFaction(factionIndex)
 end
 
 local function uRep_LoadFactions()
+    local oldFactionCount = FactionCount
     FactionCount = GetNumFactions()
-    for i = 1, FactionCount do
-        local name, _, standingId, _, _, currentRep = GetFactionInfo(i)
-        factions[name] = {}
-        factions[name].currentRep = currentRep
-        factions[name].standingId = standingId
 
+    for i = 1, FactionCount do
+        local name, _, standingId, _, barMax, currentRep, _, _, isHeader = GetFactionInfo(i)
+        local oldFaction = Factions[name]
+        if oldFaction == nil and oldFactionCount > 0 and (not isHeader) then
+            local factionChangeMessage = "Faction " .. uC(name, C.yellow) .. " found at " .. " " ..
+                                             uC(uShared_StringComma(currentRep), C.orange) .. " / " ..
+                                             uC(uShared_StringComma(barMax), C.yellow) .. " (" ..
+                                             uRep_StandingText(standingId) .. ")."
+            uShared_PrintAll("CHAT_MSG_COMBAT_FACTION_CHANGE", factionChangeMessage)
+        end
+        Factions[name] = {}
+        Factions[name].currentRep = currentRep
+        Factions[name].standingId = standingId
     end
 end
 
 local function uRep_ScanAndReport()
-    local factionCount = GetNumFactions()
-    if (factionCount ~= FactionCount) then
+    local currentFactionCount = GetNumFactions()
+    if (currentFactionCount ~= FactionCount) then
         return uRep_LoadFactions()
     end
 
-    for factionIndex = 1, factionCount do
+    for factionIndex = 1, currentFactionCount do
         uRep_ReportFaction(factionIndex)
     end
 end
@@ -105,7 +114,6 @@ eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 eventFrame:RegisterEvent("UPDATE_FACTION")
 eventFrame:SetScript("OnEvent", uRep_ScanAndReport)
 
-local function uRep_FilterGain(self, event, msg, ...)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_COMBAT_FACTION_CHANGE", function()
     return true
-end
-ChatFrame_AddMessageEventFilter("CHAT_MSG_COMBAT_FACTION_CHANGE", uRep_FilterGain)
+end)
